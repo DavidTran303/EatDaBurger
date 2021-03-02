87e4a50801a7f462104a57ae12c9d158b4c3e901
David Tran <davtran303@gmail.com>
2021-03-01 19:08:26 -0700

    pushing to heroku

--- server.js
+++ server.js
@@ -15,14 +15,20 @@ app.use(express.json());
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+let connection;
 
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: 'david0921',
   database: 'burger_db',
 });
+}
+
 
 connection.connect((err) => {
   if (err) {
